1f1fd88904739cd23abcf51497a192abb60285ba
Angela Lo <angelawenlo@gmail.com>
2019-07-02 20:09:03 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -47,6 +47,5 @@ class MusicLibraryController
     if artist = Artist.find_by_name(input)
       artist.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
         puts "#{i}. #{s.name} - #{s.genre.name}"
-    binding.pry
   end
 end
